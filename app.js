bc066daff63334b311bed7fd2122798201adb0c6
Khangnguyen1331 <khangb2014574@student.ctu.edu.vn>
2024-02-22 13:10:06 +0700

    Dinh nghia cac route tai  nguyen cho contact

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require("cors");
-//const contactsRouter = require("./app/routes/contact.route");
+const contactsRouter = require("./app/routes/contact.route");
 //const ApiError = require("./app/api-error");
 
 const app = express();
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 ge || "Internal Server Error",
  
-//app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book application."});
